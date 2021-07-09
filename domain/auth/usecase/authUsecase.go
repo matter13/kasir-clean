b2946e024180fe72b4c68beee84a8ddb6bcad7e7
matter13 <natanaelmc60@gmail.com>
2021-07-09 08:56:35 +0700

    Fix auth Logic

--- domain/auth/usecase/authUsecase.go
+++ domain/auth/usecase/authUsecase.go
@@ -4,9 +4,6 @@ import (
 	"kasir-clean/domain/auth/models"
 )
 
-type Response struct {
-	Pesan string
-}
 type AuthEntity struct {
 	authRepo models.AuthRepository
 }
@@ -17,15 +14,17 @@ func NewAuthEntity(a models.AuthRepository) models.AuthEntity {
 	}
 }
 
-func (be *AuthEntity) Cek(username string, password string) (Response, error) {
+func (be *AuthEntity) Cek(username, password string) (models.Response, error) {
 
 	auth, err := be.authRepo.Cek(username, password)
 	if err != nil {
-		return Response{Pesan: "Maaf"}, err
+		return models.Response{Pesan: "Maaf Username Password salah"}, err
 	} else if username != auth.Username {
-		return Response{Pesan: "Maap"}, err
-	} else {
-		return Response{Pesan: "Username & Pw benar"}, err
+		return models.Response{Pesan: "Username Tidak Ditemukan"}, err
+	} else if username == auth.Username && password == auth.Password {
+		return models.Response{Pesan: "Username & Pw benar"}, err
+	}else{
+		return models.Response{Pesan: "Unauthorized"},err
 	}
 	//return auth, err
 }
